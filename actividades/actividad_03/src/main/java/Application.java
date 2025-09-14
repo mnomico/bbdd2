ba195f91f37eefe5d59958b319ed204350b69282
mnomico <nomicomateou@gmail.com>
2025-09-14 16:12:26 -0300

    move Application.java

--- actividades/actividad_03/src/main/java/Application.java
+++
@@ -1,8 +0,0 @@
-import com.unluki.views.*;
-
-public class Application {
-    public static void main(String[] args) {
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.mostrarMenu();
-    }
-}
